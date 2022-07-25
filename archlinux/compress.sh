36d4812e8e617ccd7a71e4e0f206ef6be970d60f
justforlxz <justforlxz@gmail.com>
2022-07-25 17:49:32 +0800

    fix compress

--- archlinux/compress.sh
+++ archlinux/compress.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd $GITHUB_WORKSPACE
-repo=$2
+repo=$1
 
 mkdir ${repo}-git
-rsync -a . ${repo}-git --exclude "${repo}-git"
+rsync -av . ${repo}-git --exclude ${repo}-git
 rm -rf ${repo}-git/archlinux
+ls
 tar -czf archlinux/source.tar.gz ${repo}-git
 echo "deepin_source_name=${repo}-git" >> archlinux/PKGBUILD
