41690412b657452f8a2157bf96d77b459bbdd905
justforlxz <justforlxz@gmail.com>
2022-07-27 11:03:14 +0800

    update commit

--- archlinux/commit.sh
+++ archlinux/commit.sh
@@ -7,15 +7,17 @@ git clone git@github.com:deepin-community/arch-dde-repo.git
 cd .pkgs
 
 for pkg in *.zst; do
+    cd $1/.pkgs
     package=$(pacman -Qq $pkg | awk '{print $1}')
     gpg --detach-sign --default-key builder $pkg
     cp -a $pkg ../arch-dde-repo
     cp $pkg.sig ../arch-dde-repo
-    $(cd ../arch-dde-repo && repo-add -p -R ../arch-dde-repo/deepin.db.tar.xz $pkg)
+    cd ../arch-dde-repo
+    repo-add -p -R deepin.db.tar.xz $pkg
 done
 
 cd ../arch-dde-repo
 
 git add .
-git commit --amend
+git commit --amend --no-edit
 git push -f
