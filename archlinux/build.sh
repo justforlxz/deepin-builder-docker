a89356b59f4d4f976fbc0db68ad2dee98d75444c
justforlxz <justforlxz@gmail.com>
2022-08-10 14:59:02 +0800

    update

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -4,6 +4,8 @@ cd $1
 repo=$2
 version=$3
 
+pacman -Syyu
+
 sed -i "/pkgver=/cpkgver=${version}" archlinux/PKGBUILD
 sed -i "s|MAKEFLAGS=.*|MAKEFLAGS=-j$(nproc)|" /etc/makepkg.conf
 useradd -m user
@@ -11,7 +13,7 @@ mkdir /home/user/${repo}
 rsync -a archlinux/* /home/user/${repo}
 chown -R user /home/user/${repo}
 cd /home/user/${repo}
-  deepin-x86_64-build -- -U user
+  deepin-x86_64-build -c -- -U user
   mkdir $1/.pkgs
   cp /home/user/${repo}/*.zst $1/.pkgs
 
