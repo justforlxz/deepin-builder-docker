3f51cab1c3c1ba9fe558a90c7c9d6965efcdd80d
justforlxz <justforlxz@gmail.com>
2022-08-18 13:19:36 +0800

    include deb src

--- debian/commit.sh
+++ debian/commit.sh
@@ -4,7 +4,8 @@ cd $1
 
 git clone git@github.com:deepin-community/debian-sid-dde-repo.git
 
-find .pkgs -name "*.deb" | xargs -I {} reprepro --delete -b debian-sid-dde-repo -C main includedeb sid {}
+find .pkgs -name "*.deb" | xargs -I {} reprepro -b debian-sid-dde-repo -C main includedeb sid {}
+find .pkgs -name "*.dsc" | xargs -I {} reprepro -b debian-sid-dde-repo -C main includedsc sid {}
 cd debian-sid-dde-repo
 git add .
 git commit --amend --no-edit
