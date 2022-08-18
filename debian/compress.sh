ca1fddcb741f5108f72fd51c9b0cceedf08fa6b9
justforlxz <justforlxz@gmail.com>
2022-08-18 13:28:03 +0800

    update

--- debian/compress.sh
+++ debian/compress.sh
@@ -3,6 +3,7 @@
 cd $1
 repo=$2
 version=$3
+rebuild=${1:=1}
 
 PROJECT_NAME=$(dpkg-parsechangelog -S Source)
 new_dir=$PROJECT_NAME-$version
@@ -11,7 +12,7 @@ rsync -a . $new_dir --exclude $new_dir
 cd $new_dir
   rm -rf $new_dir
   rm -rf .git
-  dch -M -bv "$version-1" "update"
+  dch -M -bv "$version-$rebuild" "update"
   if [ -f ./debian/source/format ]; then sed -i 's/native/quilt/g' './debian/source/format'; fi
   dh_make --createorig -sy || true
   dpkg-source -b ./
